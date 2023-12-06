2747fd3f678e062e744713da1990a3c0087d60e4
joregete <joregete.666@gmail.com>
2023-12-06 13:13:07 +0100

    check what happens when messages have \n

--- src/benchpress_parser.py
+++ src/benchpress_parser.py
@@ -1,49 +1,39 @@
-import re, os
+import re, os, datetime
 from pprint import pprint
 
+# the return value of the parser function looks like this:
+# [message1 : dict, message2 : dict, message3 : dict, ...]
+
+# each message is a dictionary with the following keys:
+# date: datetime.date
+# time: datetime.time
+# sender: string
+# message: string
+
 def parser(file):
-    chat_data = [] # this'll be a list of dictionaries
+    chat_data = [] # list of dictionaries, each dictionary representing a message
     
     for line in file:
         if 'end-to-end encrypted' in line or 'created group' in line:
             continue
-
-        match = re.match(r'(\d{1,2}/\d{1,2}/\d{2,4}), (\d{1,2}:\d{2}\s[APM]{2}) - (.*?): (.*)', line)
+        line = line.replace('\u202f', ' ') # remove non-breaking spaces
+        pattern = r"(\d{1,2}/\d{1,2}/\d{2}), (\d{1,2}:\d{2}\s[APM]{2}) - (.*?): (.*)"
+        match = re.match(pattern, line)
         if match:
-            date, time, sender, message = match.groups()
+            date_str, time_str, sender, message = match.groups()
 
-            if "<Media omitted>" in message:
+            if "<Media omitted>" in message: # for chats exported without audio/files support, skip these lines
                 continue
 
-            if not sender: # sender is not in contacts, so it's a phone number
-                sender_match = re.search(r'\+\d+', line)
-                if sender_match:
-                    sender = sender_match.group()
-
-            chat_data.append({
-                'date': date,
-                'time': time,
-                'sender': sender,
-                'message': message
-            })
-
+            chat_data.append({'date': datetime.strptime(date_str, '%d/%m/%y'), 'time': datetime.strptime(time_str, '%I:%M %p').time(), 
+                              'sender': sender, 'message': message})
     return chat_data
 
-# # Usage
-# chat_filename = 'sample.txt'
-# parsed_chat = parser(chat_filename)
-# for message in parsed_chat:
-#     print(message)
-
-
-
 if __name__ == "__main__":
-
-    src_folder = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(src_folder, '..', 'resources', 'sample.txt')  # a bit dumbo, but it works (if it works, it aint dumb)
+    file_path = os.path.join(os.path.dirname(__file__), '..', 'resources', 'sample.txt')
 
     with open(file_path, 'r', encoding='utf-8') as file:
-        content = file.read()
-        chat_data = parser(content)
-        pprint(chat_data)
-        
+        messages = parser(file)
+    
+    for message in messages:
+        pprint(message)
